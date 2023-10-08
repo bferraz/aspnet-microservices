b9680444e3031b4b586f75fd715f9503abd9cb10
bferraz <b20-ferraz@hotmail.com>
2023-10-08 09:47:02 -0300

    codificação

--- src/Services/Ordering/Ordering.Application/Contracts/Persistence/IAsyncRepository.cs
+++ src/Services/Ordering/Ordering.Application/Contracts/Persistence/IAsyncRepository.cs
@@ -7,11 +7,11 @@ namespace Ordering.Application.Contracts.Persistence
     {
         Task<IReadOnlyList<T>> GetAllAsync();
         Task<IReadOnlyList<T>> GetAsync(Expression<Func<T, bool>> predicate);
-        Task<IReadOnlyList<T>> getAsync(Expression<Func<T, bool>> predicate = null,
+        Task<IReadOnlyList<T>> GetAsync(Expression<Func<T, bool>> predicate = null,
                                         Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
                                         string includeString = null,
                                         bool disableTracking = true);
-        Task<IReadOnlyList<T>> getAsync(Expression<Func<T, bool>> predicate = null,
+        Task<IReadOnlyList<T>> GetAsync(Expression<Func<T, bool>> predicate = null,
                                         Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
                                         List<Expression<Func<T, object>>> includes = null,
                                         bool disableTracking = true);
